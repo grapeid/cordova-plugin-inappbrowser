32af43e472a671a781bb9a3b719a61eed4daa872
Maksim Chemerisiuk <chemerisuk@gmail.com>
2022-11-18 13:25:38 +0300

    Implement clearCookies for android

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -335,6 +335,25 @@ public class InAppBrowser extends CordovaPlugin {
             pluginResult.setKeepCallback(true);
             this.callbackContext.sendPluginResult(pluginResult);
         }
+        else if (action.equals("clearCookies")) {
+            CookieManager cookieMgr = CookieManager.getInstance();
+            String domainName = args.optString(0);
+            if (domainName.isEmpty()) {
+                cookieMgr.removeAllCookies(null);
+            } else {
+                // Add https:// protocol to read all secure cookies too
+                // https://stackoverflow.com/questions/30310627/reading-secure-cookies-in-android-webview
+                String cookieString = cookieMgr.getCookie("https://" + domainName);
+                // Expire cookie one by one
+                for (String cookie : cookieString.split(";")) {
+                    String cookieName = cookie.split("=")[0];
+                    // https://stackoverflow.com/questions/23419913/android-webview-remove-cookies-from-specific-domain/23521188#23521188
+                    cookieMgr.setCookie("https://." + domainName, cookieName + "=; Max-Age=-1");
+                }
+            }
+            cookieMgr.flush();
+            callbackContext.success();
+        }
         else {
             return false;
         }
